82b131b0392245adf333781c93869bc02158a3fb
Fdeligans <fabien.deligans@ingeli.fr>
2020-11-11 11:41:13 +0100

    init

---
+++ CandidaturesCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CandidaturesCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
